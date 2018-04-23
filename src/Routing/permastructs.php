063f724ab8be1395cd2ae4e9e3cb90c238e95e44
Jerome <jerome@metabolism.fr>
2018-04-23 01:39:00 -0700

    fix multilangue

--- src/Routing/permastructs.php
+++ src/Routing/permastructs.php
@@ -10,29 +10,30 @@ use Symfony\Component\Routing\Route,
 global $wp_rewrite, $wp_post_types, $_config;
 
 $controller_name = $_config->get('extra_permastructs.controller', 'MainController');
-$locale = $_config->get('multisite.multilangue') ? '{_locale}': '';
+$_locale = ($_config->get('multisite.multilangue') && !$_config->get('multisite.subdomain_install')) ? '{_locale}{_separator}': '';
 
 $collection = new RouteCollection();
 
-$addRoute = function( $name, $struct, $is_archive=false ) use($controller_name, $locale, $collection, $wp_rewrite)
+$addRoute = function( $name, $struct, $is_archive=false ) use($controller_name, $_locale, $collection, $wp_rewrite)
 {
 	$name = str_replace('_structure', '', $name);
 
 	$controller = 'App\Controller\\'.$controller_name.'::'.str_replace(' ', '',lcfirst(ucwords(str_replace('_', ' ', $name))).'Action');
 	$path = str_replace('%/', '}', str_replace('/%', '/{', $struct));
-	$path = preg_replace('/\%$/', '}', preg_replace('/^\%/', '{', $path));
+	$path = preg_replace('/\%$/', '}', preg_replace('/^\%/', '/{', $path));
+	$path = ltrim($path, '/');
 
-	$route = new Route( $locale.$path, ['_controller'=>$controller]);
+	$route = new Route( $_locale.$path, ['_controller'=>$controller]);
 	$collection->add('wp_'.$name, $route);
 
 	if( $is_archive )
 	{
-		$route = new Route( $locale.$path.'/'.$wp_rewrite->pagination_base.'/{page}', ['_controller'=>$controller]);
+		$route = new Route( $_locale.$path.'/'.$wp_rewrite->pagination_base.'/{page}', ['_controller'=>$controller]);
 		$collection->add('wp_'.$name.'_paged', $route);
 	}
 };
 
-$addRoute('front', '/');
+$addRoute('front', '');
 
 foreach ($wp_rewrite->extra_permastructs as $name=>$permastruct)
 {
@@ -54,6 +55,18 @@ foreach ($wp_post_types as $wp_post_type)
 foreach (['author_structure', 'search_structure', 'page_structure'] as $name)
 {
 	$addRoute($name, $wp_rewrite->$name);
+}
+
+if( !empty($_locale) )
+{
+	$locales = [];
+	$sites = get_sites();
+
+	foreach ($sites as $site)
+		$locales[] = str_replace('/', '', $site->path);
+
+	$collection->addRequirements(['_separator'=>'/?', '_locale'=>implode('|', $locales)]);
+	$collection->addDefaults(['_separator'=>'/', '_locale'=>$_config->get('multisite.default_locale', 'en')]);
 }
 
 return $collection;
