8076371d798b867aa490919fc2cda88179483f6f
Nicolas RIVIERE <nico.riviere78@gmail.com>
2019-07-30 12:02:44 +0200

    SVG condition

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -237,7 +237,7 @@ class Image extends Entity
 
 		$src_ext = pathinfo($this->src, PATHINFO_EXTENSION);
 
-		if( $this->mime_type == 'image/svg+xml' )
+		if( $this->mime_type == 'image/svg+xml' || $this->mime_type == 'image/svg' )
 			return $this->src;
 
 		if( $ext == null )
