65ffeed7e5e5baf9fd9937c93766ef4faa0212cd
Jerome Barbato <jerome.barbato@gmail.com>
2022-10-14 12:13:21 +0200

    bugfix

--- src/WordpressBundle.php
+++ src/WordpressBundle.php
@@ -4,127 +4,122 @@ namespace Metabolism\WordpressBundle;
 
 use Env\Env;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\Routing\Router;
-use function Env\env;
 
 class WordpressBundle extends Bundle
 {
-    private $root_dir;
-    private $public_dir;
-    private $log_dir;
-    private $router;
+	private $root_dir;
+	private $public_dir;
+	private $log_dir;
+	private $router;
 
-    private $wp_path = "public/edition/";
+	private $wp_path = "public/edition/";
 
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
+	public function getPath(): string
+	{
+		return \dirname(__DIR__);
+	}
 
-    public function boot()
-    {
-        Env::$options = Env::USE_ENV_ARRAY;
+	public function boot()
+	{
+		Env::$options = Env::USE_ENV_ARRAY;
 
 		$kernel = $this->container->get('kernel');
 
-        $this->log_dir = $kernel->getLogDir();
-        $this->root_dir = $kernel->getProjectDir();
-        $this->router = $this->container->get('router');
+		$this->log_dir = $kernel->getLogDir();
+		$this->root_dir = $kernel->getProjectDir();
+		$this->router = $this->container->get('router');
 
-        $this->public_dir = $this->root_dir.(is_dir($this->root_dir.'/public') ? '/public' : '/web');
+		$this->public_dir = $this->root_dir.(is_dir($this->root_dir.'/public') ? '/public' : '/web');
 
-        $this->resolveServer();
-        $this->loadWordpress();
-    }
+		$this->resolveServer();
+		$this->loadWordpress();
+	}
 
 
-    private function resolveServer(){
+	private function resolveServer(){
 
 		$context = $this->router->getContext();
 
-        if( !isset($_SERVER['HTTP_HOST']) ) {
+		if( !isset($_SERVER['HTTP_HOST']) ) {
 
-            if( $host = env('WP_MULTISITE') ){
+			$_SERVER['SERVER_PORT'] = $context->isSecure() ? $context->getHttpsPort() : $context->getHttpPort();
 
-                $_SERVER['HTTP_HOST'] = $host;
-            }
-            elseif( $host = ($_SERVER['SERVER_NAME']??false) ){
+			if( $context->isSecure() ){
 
-                $_SERVER['HTTP_HOST'] = $host;
-            }
-            else{
+				$_SERVER['REQUEST_SCHEME'] = 'https';
+				$_SERVER['HTTPS'] = 'on';
+				$_SERVER['HTTP_HOST'] = $context->getHost().($context->getHttpsPort()!=443?':'.$context->getHttpsPort():'');
+			}
+			else{
 
-	            $_SERVER['SERVER_PORT'] = $context->isSecure() ? $context->getHttpsPort() : $context->getHttpPort();
-
-				if( $context->isSecure() )
-					$_SERVER['HTTP_HOST'] = $context->getHost().($context->getHttpsPort()!=443?':'.$context->getHttpsPort():'');
-	            else
-		            $_SERVER['HTTP_HOST'] = $context->getHost().($context->getHttpPort()!=80?':'.$context->getHttpPort():'');
-            }
-        }
+				$_SERVER['REQUEST_SCHEME'] = 'http';
+				$_SERVER['HTTPS'] = 'off';
+				$_SERVER['HTTP_HOST'] = $context->getHost().($context->getHttpPort()!=80?':'.$context->getHttpPort():'');
+			}
+		}
 
 		if( !isset($_SERVER['REMOTE_ADDR']) && php_sapi_name() == "cli" )
 			$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 	}
 
-    public static function loadPlugins (){
+	public static function loadPlugins (){
 
-        $plugins = scandir(__DIR__.'/Plugin');
+		$plugins = scandir(__DIR__.'/Plugin');
 
-        foreach($plugins as $plugin){
+		foreach($plugins as $plugin){
 
-            if( !in_array($plugin, ['.','..']) )
-            {
-                $classname = '\Metabolism\WordpressBundle\Plugin\\'.str_replace('.php', '', $plugin);
+			if( !in_array($plugin, ['.','..']) )
+			{
+				$classname = '\Metabolism\WordpressBundle\Plugin\\'.str_replace('.php', '', $plugin);
 
-                if( class_exists($classname) )
-                    new $classname();
-            }
-        }
-    }
+				if( class_exists($classname) )
+					new $classname();
+			}
+		}
+	}
 
-    public static function isLoginUrl(){
+	public static function isLoginUrl(){
 
-        $uri = explode('/', $_SERVER['SCRIPT_NAME']);
-        $page = end($uri);
+		$uri = explode('/', $_SERVER['SCRIPT_NAME']);
+		$page = end($uri);
 
-        return in_array( $page, ['wp-login.php', 'wp-signup.php'] );
-    }
+		return in_array( $page, ['wp-login.php', 'wp-signup.php'] );
+	}
 
-    /**
-     * 	@see wp-includes/class-wp.php, main function
-     */
-    private function loadWordpress(){
+	/**
+	 * 	@see wp-includes/class-wp.php, main function
+	 */
+	private function loadWordpress(){
 
-        if( !file_exists($this->public_dir.'/wp-config.php') )
-            return;
+		if( !file_exists($this->public_dir.'/wp-config.php') )
+			return;
 
-        if (!defined('WP_DEBUG_LOG'))
-            define('WP_DEBUG_LOG', realpath($this->log_dir . '/wp-errors.log'));
+		if (!defined('WP_DEBUG_LOG'))
+			define('WP_DEBUG_LOG', realpath($this->log_dir . '/wp-errors.log'));
 
-        $composer = $this->root_dir.'/composer.json';
+		$composer = $this->root_dir.'/composer.json';
 
-        // get WordPress path
-        if( !is_dir($this->root_dir.'/'.$this->wp_path) && file_exists($composer) ){
+		// get WordPress path
+		if( !is_dir($this->root_dir.'/'.$this->wp_path) && file_exists($composer) ){
 
-            $composer = json_decode(file_get_contents($composer), true);
-            $installer_paths= $composer['extra']['installer-paths']??[];
+			$composer = json_decode(file_get_contents($composer), true);
+			$installer_paths= $composer['extra']['installer-paths']??[];
 
-            foreach ($installer_paths as $installer_path=>$types){
+			foreach ($installer_paths as $installer_path=>$types){
 
-                if( in_array("type:wordpress-core", $types) )
-                    $this->wp_path = $installer_path;
-            }
-        }
+				if( in_array("type:wordpress-core", $types) )
+					$this->wp_path = $installer_path;
+			}
+		}
 
-        // start loading WordPress core without theme support
-        $wp_load_script = $this->root_dir.'/'.$this->wp_path.'wp-load.php';
+		// start loading WordPress core without theme support
+		$wp_load_script = $this->root_dir.'/'.$this->wp_path.'wp-load.php';
 
-        if( !file_exists($wp_load_script) )
-            return;
+		if( !file_exists($wp_load_script) )
+			return;
 
-        include $wp_load_script;
+		include $wp_load_script;
 
-        remove_action( 'template_redirect', 'redirect_canonical' );
-    }
+		remove_action( 'template_redirect', 'redirect_canonical' );
+	}
 }
