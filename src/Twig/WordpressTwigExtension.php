cba82caaad73a92499107c598497c4f462c2cce3
Jerome Barbato <jerome.barbato@gmail.com>
2023-04-12 15:26:51 +0200

    bugfix

--- src/Twig/WordpressTwigExtension.php
+++ src/Twig/WordpressTwigExtension.php
@@ -15,79 +15,79 @@ use Metabolism\WordpressBundle\Factory\PostFactory;
 use Metabolism\WordpressBundle\Factory\TermFactory;
 
 use Twig\Extension\AbstractExtension,
-	Twig\TwigFilter,
-	Twig\TwigFunction;
+    Twig\TwigFilter,
+    Twig\TwigFunction;
 use Twig\Markup;
 
 class WordpressTwigExtension extends AbstractExtension{
 
-	/**
-	 * @return array
-	 */
-	public function getFilters(): array
-	{
-		return [
+    /**
+     * @return array
+     */
+    public function getFilters(): array
+    {
+        return [
             new TwigFilter( 'handle', 'sanitize_title' ),
             new TwigFilter( 'placeholder', [$this, 'placeholder'] ),
-			new TwigFilter( 'more', [$this, 'more'] ),
-			new TwigFilter( 'resize', [$this, 'resize'] ),
-			new TwigFilter( 'picture', [$this, 'picture'] ),
-			new TwigFilter( 'figure', [$this, 'figure'] ),
-			new TwigFilter( 'stripshortcodes','strip_shortcodes' ),
-			new TwigFilter( 'trim_words','wp_trim_words' ),
-			new TwigFilter( 'function', [$this, 'execFunction'] ),
-			new TwigFilter( 'excerpt','wp_trim_words' ),
-			new TwigFilter( 'sanitize','sanitize_title' ),
-			new TwigFilter( 'base64_encode','base64_encode' ),
-			new TwigFilter( 'base64_decode','base64_decode' ),
-			new TwigFilter( 'shortcodes', [$this, 'doShortcode'] ),
-			new TwigFilter( 'wpautop','wpautop' ),
-			new TwigFilter( 'array',[$this, 'toArray'] ),
-			new TwigFilter( 'file_exists',[$this, 'fileExists'] ),
-		];
-	}
+            new TwigFilter( 'more', [$this, 'more'] ),
+            new TwigFilter( 'resize', [$this, 'resize'] ),
+            new TwigFilter( 'picture', [$this, 'picture'] ),
+            new TwigFilter( 'figure', [$this, 'figure'] ),
+            new TwigFilter( 'stripshortcodes','strip_shortcodes' ),
+            new TwigFilter( 'trim_words','wp_trim_words' ),
+            new TwigFilter( 'function', [$this, 'execFunction'] ),
+            new TwigFilter( 'excerpt','wp_trim_words' ),
+            new TwigFilter( 'sanitize','sanitize_title' ),
+            new TwigFilter( 'base64_encode','base64_encode' ),
+            new TwigFilter( 'base64_decode','base64_decode' ),
+            new TwigFilter( 'shortcodes', [$this, 'doShortcode'] ),
+            new TwigFilter( 'wpautop','wpautop' ),
+            new TwigFilter( 'array',[$this, 'toArray'] ),
+            new TwigFilter( 'file_exists',[$this, 'fileExists'] ),
+        ];
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getFunctions(): array
-	{
-		return [
-			new TwigFunction( 'placeholder', [$this, 'generatePlaceholder'] ),
-			new TwigFunction( 'pixel', [$this, 'generatePixel'] ),
-			new TwigFunction( 'fn', [$this, 'execFunction'] ),
-			new TwigFunction( 'function', [$this, 'execFunction'] ),
-			new TwigFunction( 'action', [$this, 'doAction'] ),
-			new TwigFunction( 'shortcode', 'do_shortcode' ),
-			new TwigFunction( 'login_url', 'wp_login_url' ),
-			new TwigFunction( 'home_url', 'get_home_url' ),
-			new TwigFunction( 'search_form', 'get_search_form' ),
-			new TwigFunction( 'archive_url', 'get_post_type_archive_link' ),
-			new TwigFunction( 'attachment_url', 'wp_get_attachment_url' ),
-			new TwigFunction( 'post_url', [$this, 'getPermalink'] ),
-			new TwigFunction( 'term_url', [$this, 'getTermLink'] ),
-			new TwigFunction( 'bloginfo', 'get_bloginfo' ),
-			new TwigFunction( 'dynamic_sidebar', function($id){ return $this->getOutput('dynamic_sidebar', [$id]); }, ['is_safe' => array('html')]  ),
-			new TwigFunction( 'comment_form', function($post_id, $args=[]){ return $this->getOutput('comment_form', [$args, $post_id]); }, ['is_safe' => array('html')]  ),
-			new TwigFunction( 'is_active_sidebar', 'is_active_sidebar' ),
-			new TwigFunction( '_e', 'translate' ),
-			new TwigFunction( '_x', '_x' ),
-			new TwigFunction( '_ex', '_ex' ),
-			new TwigFunction( '_nx', '_nx' ),
-			new TwigFunction( '_n_noop', '_n_noop' ),
-			new TwigFunction( '_nx_noop', '_nx_noop' ),
-			new TwigFunction( '_n', '_n' ),
-			new TwigFunction( '__', 'translate' ),
-			new TwigFunction( 'translate', 'translate' ),
-			new TwigFunction( 'translate_nooped_plural', 'translate_nooped_plural' ),
-			new TwigFunction( 'wp_head', function(){ return @$this->getOutput('wp_head'); }, ['is_safe' => array('html')]  ),
-			new TwigFunction( 'wp_footer', function(){ return @$this->getOutput('wp_footer'); }, ['is_safe' => array('html')]  ),
-			new TwigFunction( 'Post', function($id){ return PostFactory::create($id); } ),
-			new TwigFunction( 'User', function($id){ return Factory::create($id, 'user'); } ),
-			new TwigFunction( 'Term', function($id){ return TermFactory::create($id); } ),
-			new TwigFunction( 'Image', function($id){ return Factory::create($id, 'image'); } )
-		];
-	}
+    /**
+     * @return array
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction( 'placeholder', [$this, 'generatePlaceholder'] ),
+            new TwigFunction( 'pixel', [$this, 'generatePixel'] ),
+            new TwigFunction( 'fn', [$this, 'execFunction'] ),
+            new TwigFunction( 'function', [$this, 'execFunction'] ),
+            new TwigFunction( 'action', [$this, 'doAction'] ),
+            new TwigFunction( 'shortcode', 'do_shortcode' ),
+            new TwigFunction( 'login_url', 'wp_login_url' ),
+            new TwigFunction( 'home_url', 'get_home_url' ),
+            new TwigFunction( 'search_form', 'get_search_form' ),
+            new TwigFunction( 'archive_url', 'get_post_type_archive_link' ),
+            new TwigFunction( 'attachment_url', 'wp_get_attachment_url' ),
+            new TwigFunction( 'post_url', [$this, 'getPermalink'] ),
+            new TwigFunction( 'term_url', [$this, 'getTermLink'] ),
+            new TwigFunction( 'bloginfo', 'get_bloginfo' ),
+            new TwigFunction( 'dynamic_sidebar', function($id){ return $this->getOutput('dynamic_sidebar', [$id]); }, ['is_safe' => array('html')]  ),
+            new TwigFunction( 'comment_form', function($post_id, $args=[]){ return $this->getOutput('comment_form', [$args, $post_id]); }, ['is_safe' => array('html')]  ),
+            new TwigFunction( 'is_active_sidebar', 'is_active_sidebar' ),
+            new TwigFunction( '_e', 'translate' ),
+            new TwigFunction( '_x', '_x' ),
+            new TwigFunction( '_ex', '_ex' ),
+            new TwigFunction( '_nx', '_nx' ),
+            new TwigFunction( '_n_noop', '_n_noop' ),
+            new TwigFunction( '_nx_noop', '_nx_noop' ),
+            new TwigFunction( '_n', '_n' ),
+            new TwigFunction( '__', 'translate' ),
+            new TwigFunction( 'translate', 'translate' ),
+            new TwigFunction( 'translate_nooped_plural', 'translate_nooped_plural' ),
+            new TwigFunction( 'wp_head', function(){ return @$this->getOutput('wp_head'); }, ['is_safe' => array('html')]  ),
+            new TwigFunction( 'wp_footer', function(){ return @$this->getOutput('wp_footer'); }, ['is_safe' => array('html')]  ),
+            new TwigFunction( 'Post', function($id){ return PostFactory::create($id); } ),
+            new TwigFunction( 'User', function($id){ return Factory::create($id, 'user'); } ),
+            new TwigFunction( 'Term', function($id){ return TermFactory::create($id); } ),
+            new TwigFunction( 'Image', function($id){ return Factory::create($id, 'image'); } )
+        ];
+    }
 
 
     /**
@@ -98,8 +98,8 @@ class WordpressTwigExtension extends AbstractExtension{
      */
     public function toArray($arr ) {
 
-		return (array)$arr;
-	}
+        return (array)$arr;
+    }
 
 
     /**
@@ -109,8 +109,8 @@ class WordpressTwigExtension extends AbstractExtension{
      */
     public function fileExists($path ) {
 
-		return substr($path, 0, 4) == 'http' || file_exists(BASE_URI . PUBLIC_DIR . $path);
-	}
+        return substr($path, 0, 4) == 'http' || file_exists(BASE_URI . PUBLIC_DIR . $path);
+    }
 
 
     /**
@@ -125,6 +125,8 @@ class WordpressTwigExtension extends AbstractExtension{
 
         if( $h == 0 )
             $h = $w;
+        elseif( $w == 0 )
+            $w = $h;
 
         $img = imagecreatetruecolor($w, $h);
         imagetruecolortopalette($img, false, 1);
@@ -137,7 +139,7 @@ class WordpressTwigExtension extends AbstractExtension{
         $imagedata = ob_get_contents();
         ob_end_clean();
 
-       return 'data:image/png;base64,' . base64_encode($imagedata);
+        return 'data:image/png;base64,' . base64_encode($imagedata);
     }
 
 
@@ -195,19 +197,19 @@ class WordpressTwigExtension extends AbstractExtension{
         if( !is_array($params) )
             $params = [];
 
-	    if( is_string($image) && !empty($image) ){
+        if( is_string($image) && !empty($image) ){
 
             $image = new Image($image);
         }
         elseif( is_array($image) && !empty($image['url']??'') ){
 
-	        if( !$alt && isset($image['alt']) )
+            if( !$alt && isset($image['alt']) )
                 $alt = $image['alt'];
 
             $image = new Image($image['url']);
         }
 
-	    if( !$image instanceof Image ){
+        if( !$image instanceof Image ){
 
             if( !$height )
                 $height = $width;
@@ -229,7 +231,7 @@ class WordpressTwigExtension extends AbstractExtension{
             $html .= '</figure>';
         }
 
-		return new \Twig\Markup($html, 'UTF-8');
+        return new \Twig\Markup($html, 'UTF-8');
     }
 
 
@@ -256,17 +258,17 @@ class WordpressTwigExtension extends AbstractExtension{
     }
 
 
-	/**
-	 * Return html
-	 *
-	 * @param $text
-	 * @return Markup
-	 */
+    /**
+     * Return html
+     *
+     * @param $text
+     * @return Markup
+     */
     public function doShortcode($text)
     {
         $html = do_shortcode($text);
 
-		return new \Twig\Markup($html, 'UTF-8');
+        return new \Twig\Markup($html, 'UTF-8');
     }
 
 
@@ -281,58 +283,58 @@ class WordpressTwigExtension extends AbstractExtension{
         ob_start();
         call_user_func_array($function, $args);
 
-		$data = ob_get_contents();
-	    ob_end_clean();
+        $data = ob_get_contents();
+        ob_end_clean();
 
         return $data;
     }
 
 
-	/**
-	 * @param object|int|string $term
-	 * @param string $taxonomy
-	 * @param bool|string $meta
-	 * @return false|string
+    /**
+     * @param object|int|string $term
+     * @param string $taxonomy
+     * @param bool|string $meta
+     * @return false|string
      */
-	public function getTermLink( $term, $taxonomy = 'category', $meta=false )
-	{
+    public function getTermLink( $term, $taxonomy = 'category', $meta=false )
+    {
 
-		if( $meta ){
+        if( $meta ){
 
-			$args = array(
-				'meta_query' => array(
-					array(
-						'key'       => $meta,
-						'value'     => $term,
-						'compare'   => 'LIKE'
-					)
-				),
-				'number'  => 1,
-				'taxonomy'  => $taxonomy,
-			);
+            $args = array(
+                'meta_query' => array(
+                    array(
+                        'key'       => $meta,
+                        'value'     => $term,
+                        'compare'   => 'LIKE'
+                    )
+                ),
+                'number'  => 1,
+                'taxonomy'  => $taxonomy,
+            );
 
-			$terms = get_terms( $args );
+            $terms = get_terms( $args );
 
-			if( count($terms) )
-				$term = $terms[0];
-		}
-
-
-		$link = get_term_link($term, $taxonomy);
-
-		if( !is_string($link) )
-			return false;
-
-		return $link;
-	}
+            if( count($terms) )
+                $term = $terms[0];
+        }
 
 
-	/**
-	 * @param $content
-	 * @return array
+        $link = get_term_link($term, $taxonomy);
+
+        if( !is_string($link) )
+            return false;
+
+        return $link;
+    }
+
+
+    /**
+     * @param $content
+     * @return array
      */
-	public function more( $content )
-	{
+    public function more( $content )
+    {
         if ( preg_match( '/<!--more(.*?)?-->/', $content, $matches ) ) {
             if ( has_block( 'more', $content ) ) {
                 // Remove the core/more block delimiters. They will be left over after $content is split up.
@@ -348,47 +350,47 @@ class WordpressTwigExtension extends AbstractExtension{
             $paragraph = force_balance_tags($paragraph);
 
         return $content;
-	}
+    }
 
 
-	/**
-	 * @param $function_name
-	 * @return mixed
-	 */
-	public function execFunction( $function_name )
-	{
-		$args = func_get_args();
-
-		array_shift($args);
-
-		if ( is_string($function_name) )
-			$function_name = trim($function_name);
-
-		return call_user_func_array($function_name, ($args));
-	}
-
-
-	/**
-	 * @param $action_name
-	 * @param mixed ...$args
-	 * @return void
-	 */
-	public function doAction( $action_name, ...$args )
-	{
-		do_action_ref_array( $action_name, $args );
-	}
-
-
-	/**
-	 * @param $page
-	 * @param bool $by
-	 * @return false|string
+    /**
+     * @param $function_name
+     * @return mixed
      */
-	public function getPermalink( $page, $by=false )
-	{
-		switch ( $by ){
+    public function execFunction( $function_name )
+    {
+        $args = func_get_args();
 
-			case 'state':
+        array_shift($args);
+
+        if ( is_string($function_name) )
+            $function_name = trim($function_name);
+
+        return call_user_func_array($function_name, ($args));
+    }
+
+
+    /**
+     * @param $action_name
+     * @param mixed ...$args
+     * @return void
+     */
+    public function doAction( $action_name, ...$args )
+    {
+        do_action_ref_array( $action_name, $args );
+    }
+
+
+    /**
+     * @param $page
+     * @param bool $by
+     * @return false|string
+     */
+    public function getPermalink( $page, $by=false )
+    {
+        switch ( $by ){
+
+            case 'state':
 
                 if( !function_exists('get_page_by_state') )
                     return false;
@@ -396,17 +398,17 @@ class WordpressTwigExtension extends AbstractExtension{
                 $page = get_page_by_state($page);
                 break;
 
-			case 'path':
+            case 'path':
 
-				$page = get_page_by_path($page);
-				break;
+                $page = get_page_by_path($page);
+                break;
 
-			case 'title':
+            case 'title':
 
-				$page = get_page_by_title($page);
-				break;
+                $page = get_page_by_title($page);
+                break;
 
-			case 'slug':
+            case 'slug':
 
                 if( !is_array($page) or count($page) != 2 )
                     return false;
@@ -418,47 +420,47 @@ class WordpressTwigExtension extends AbstractExtension{
                     'fields' => 'ids'
                 ]);
 
-				if( count($post_ids) )
+                if( count($post_ids) )
                     $page = $post_ids[0];
-		}
+        }
 
-		if( $page ){
+        if( $page ){
 
-			$link = get_permalink($page);
+            $link = get_permalink($page);
 
-			if( !is_string($link) )
-				return false;
+            if( !is_string($link) )
+                return false;
 
-			return $link;
-		}
-		else
-			return false;
-	}
-
-
-	/**
-	 * @param $image
-	 * @param bool $params
-	 * @return Image
-	 */
-	public function placeholder($image, $params=false)
-	{
-		if(!$image instanceof Image)
-			return new Image();
-
-		return $image;
-	}
+            return $link;
+        }
+        else
+            return false;
+    }
 
 
-	/**
-	 * @param $w
-	 * @param $h
-	 * @return string
-	 */
-	public function generatePlaceholder($w, $h)
-	{
-		$image = new Image();
+    /**
+     * @param $image
+     * @param bool $params
+     * @return Image
+     */
+    public function placeholder($image, $params=false)
+    {
+        if(!$image instanceof Image)
+            return new Image();
 
-		return $image->placeholder($w, $h);
-	}
+        return $image;
+    }
+
+
+    /**
+     * @param $w
+     * @param $h
+     * @return string
+     */
+    public function generatePlaceholder($w, $h)
+    {
+        $image = new Image();
+
+        return $image->placeholder($w, $h);
+    }
 }
